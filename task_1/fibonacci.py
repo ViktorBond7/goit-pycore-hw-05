15ef53c915dc67e7caa7869008c492009c9bfaa7
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2025-03-22 22:40:32 +0100

    task-1

---
+++ task_1/fibonacci.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+def caching_fibonacci() -> Callable[[int], int]:
+    cache= {}
+   
+    def fibonacci(n: int)-> int:
+        if n <=0:
+            return 0
+        if n == 1:
+            return 1
+        if n in cache:
+            return cache[n]
+        
+        cache[n] = fibonacci(n-1) + fibonacci(n-2)
+        return cache[n]
+    
+    return fibonacci
+    
+result = caching_fibonacci()
+
+
+print(result(10))
+print(result(15))
+
+
+
+
