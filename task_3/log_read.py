058e025f886bba146bd6d822984a7add16af9d89
ViktorBond7 <141341341+ViktorBond7@users.noreply.github.com>
2025-03-24 20:46:53 +0100

    task-3 updatу

--- task_3/log_read.py
+++ task_3/log_read.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+import sys
+from colorama import Fore, Style, init
+
+init(autoreset=True)  # Автоматичне скидання кольорів після кожного рядка
 
 def parse_log_line(line: str) -> dict:
-    
     parts = line.split(" ", 3)
     date, time, level, message = parts
 
@@ -15,51 +18,74 @@ def parse_log_line(line: str) -> dict:
 def load_logs(file_path: str) -> list[dict]:
     path = Path(file_path)
     logs = []
-    with open(path, encoding="utf-8") as file:
-        for el in file: 
-            el = el.strip()  
-            res = parse_log_line(el)
-            if res:
-                logs.append(res)
+    try:
+        with open(path, encoding="utf-8") as file:
+            for el in file: 
+                el = el.strip()  
+                try:
+                    res = parse_log_line(el)
+                    if res:
+                        logs.append(res)
+                except ValueError as e:
+                    print(f"Помилка парсингу рядка: {e}")
+    
+    except FileNotFoundError:
+        print(f"Файл {file_path} не знайдено.")
 
     return logs
 
-# load_logs("task_3/log.txt")
-
-def filter_logs_by_level(logs: list[dict], level: str) -> list:
+def filter_logs_by_level(logs: list[dict], level: str)->list:
+    filtered_logs=[]
     level = level.upper()
-    res = [item for item in logs if item[level] == level]
-    return res
+    print(f"\vДеталі логів для рівня '{level}':")
+
+    logs_log = filter(lambda x: x["level"] == level, logs )
+
+    for log in logs_log:
+       filtered_logs.append(f"{log['data']} {log['time']} - {log['message']}")
+    return filtered_logs
 
 def count_logs_by_level(logs: list[dict]) -> dict:
-    total = {
+    levevs = {
         "INFO": 0,
         "WARNING": 0,
-        "DEBUG": 0,
+        "DEBUG": 0, 
         "ERROR": 0,
     }
-    for item in logs:
-        if item["level"] in total:
-            total[item["level"]] +=1
-    return total
+    for log in logs:
+        if log["level"] in levevs:
+            levevs[log["level"]] +=1
+    return levevs
 
-# print(count_logs_by_level(load_logs("task_3/log.txt")))
+def display_log_counts(counts: dict):  
+    color_map = {
+        "INFO": Fore.WHITE,
+        "DEBUG": Fore.CYAN,
+        "ERROR": Fore.MAGENTA,
+        "WARNING": Fore.YELLOW,
+    } 
+    print(f"{'Рівень логування':17} | {'Кількість'}")
+    print(f"{'-' * 17} | {'-'*10}")
 
-def display_log_counts(counts: dict):
-    print("рівень логування кількість")
     for item, value in counts.items():
-        print(f"{item:15} |{" "*3}{value}")
-    
+        print(f"{color_map[item]+ item:22} {Style.RESET_ALL}|{' '*3}{value}")
 
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("❌ Помилка: Ви не вказали шлях до файлу логів.")
+        print("Використання: python main.py path/to/logfile.log [log_level]")
+        sys.exit(1)
 
-display_log_counts(count_logs_by_level(load_logs("task_3/log.txt")))
-print(filter_logs_by_level(load_logs("task_3/log.txt", "WARNING")))
+    log_file = sys.argv[1]
+    log_level = sys.argv[2] if len(sys.argv) > 2 else None
 
-
-# if __name__=="__main":
-#     res = load_logs("task_3/log.txt")
-#     res1 = count_logs_by_level(res)
-#     display_log_counts(res1)
+    logs = load_logs(log_file)
+    log_counts = count_logs_by_level(logs)
+    display_log_counts(log_counts)
+    if log_level:
+        for log in filter_logs_by_level(logs, log_level):
+            print(log)
+        
 
 
 
